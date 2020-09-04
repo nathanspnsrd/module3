f4f47a87b0b137007a4283232eaf3f8c2c3cdb7e
Nathan Sutherland <nathan.sutherland17@gmail.com>
2020-09-04 13:00:47 -0600

    maybe

--- DjJs/djjs/src/Buttons.js
+++ DjJs/djjs/src/Buttons.js
@@ -2,6 +2,21 @@ import React from "react"
 import "./buttons.css"
 
 class Buttons extends React.Component {
+    constructor() {
+        super()
+        this.state ={
+            colors: ["white", "white", "white", "white"]
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                colors: ["black", "black", "black", "black"]
+            }
+        })
+    }
 
     render() {
         return (
