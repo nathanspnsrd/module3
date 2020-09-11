9a294e80aa2b5a2480222a9402cf84b2019f9fb4
Nathan Sutherland <nathan.sutherland17@gmail.com>
2020-09-11 13:21:34 -0600

    thank you

--- nameEntry/name-entry/src/App.js
+++ nameEntry/name-entry/src/App.js
@@ -1,11 +1,66 @@
 import React from "react"
 import "./App.css"
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: "",
+      lastName: "",
+      fullNames: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({firstName: "", lastName: ""})
+
+    let newFullName = `${this.state.firstName} ${this.state.lastName}`
+    this.setState(prevState => ({
+      fullNames: [...prevState.fullNames, newFullName]
+    }))
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Please Enter Your Name</h1>
+        <form className="form">
+          <input
+            type="text"
+            value={this.state.firstName}
+            name="firstName"
+            placeholder="First Name"
+            onChange={this.handleChange}
+            className="input"
+          />
+          <br />
+          <input
+            type="text"
+            value={this.state.lastName}
+            name="lastName"
+            placeholder="Last Name"
+            onChange={this.handleChange}
+            className="input"
+          />
+          <button className="input" onClick={this.handleSubmit}>Add Name</button>
+        </form>
+        <h1 className="newName">{this.state.firstName} {this.state.lastName}</h1>
+        <div className="names">
+          {this.state.fullNames.map(fullName => 
+            <h1>{fullName}</h1>
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
